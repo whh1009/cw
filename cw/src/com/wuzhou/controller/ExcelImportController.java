e64df37169da352cb7dc1bf9812396791009763c
whh <wanghonghui1009@hotmail.com>
2016-04-26 17:06:21 +0800

    1

---
+++ cw/src/com/wuzhou/controller/ExcelImportController.java
@@ -0,0 +1,18 @@
+package com.wuzhou.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * excel导入控制
+ * @author wanghonghui
+ *
+ */
+public class ExcelImportController extends Controller{
+	
+	/**
+	 * 图书基本信息导入页面
+	 */
+	public void baseBookImportPage() {
+		render("");
+	}
+}
